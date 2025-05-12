4761b3596c57cd252e73e22d69f5b0ac353fe79b
Yoshiyuki HINO <yhinoz@gmail.com>
2025-05-12 18:02:50 +0900

    :bug: compatible with removal of CliRunner mix_stderr parameter in Click 8.2.0
    see https://github.com/pallets/click/releases/tag/8.2.0

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from typer.testing import CliRunner
 from pipenv_uv_migrate import __version__
 from pipenv_uv_migrate.__main__ import app
 
-runner = CliRunner(mix_stderr=False)
+runner = CliRunner()
 
 
 def test_main(
